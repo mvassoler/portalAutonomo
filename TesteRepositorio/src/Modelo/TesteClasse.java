ebf2e6d091d2b91e5e1f5afe3a5693013e4f494b
mvassoler <marcosvassoler@yahoo.com.br>
2015-02-26 20:56:14 -0300

    Meu primeiro comit

---
+++ TesteRepositorio/src/Modelo/TesteClasse.java
@@ -0,0 +1,21 @@
+package Modelo;
+
+public class TesteClasse {
+	
+	private int teste;
+	
+	public TesteClasse(){
+		
+	}
+
+	public int getTeste() {
+		return teste;
+	}
+
+	public void setTeste(int teste) {
+		this.teste = teste;
+	}
+	
+	
+
+}
